3452164d13ad0da22e6b8f8e82ba8ace91d4d9ad
nour hk <nour20hk@gmail.com>
2021-10-31 21:42:10 +0200

    Update Level1.java

--- src/main/java/com/lau/group_project/Level1.java
+++ src/main/java/com/lau/group_project/Level1.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
@@ -19,6 +20,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Random;
 import java.util.regex.Matcher;
@@ -30,7 +32,6 @@ public class Level1 extends AppCompatActivity {
     LinkedList<String> images,names,options;
     int count;
     ImageView image;
-    TextView imgnry_timer;
     String right;
 
     @Override
@@ -46,97 +47,37 @@ public class Level1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level1);
 
-        imgnry_timer = (TextView) findViewById(R.id.txt_timer);
         image = (ImageView) findViewById(R.id.appImage);
-        option1 = findViewById(R.id.guess1);
-        option2 = findViewById(R.id.guess2);
-        option3 = findViewById(R.id.guess3);
-        option4 = findViewById(R.id.guess4);
+        option1 = (Button) findViewById(R.id.guess1);
+        option2 = (Button) findViewById(R.id.guess2);
+        option3 = (Button) findViewById(R.id.guess3);
+        option4 = (Button) findViewById(R.id.guess4);
 
 
         try {
+            DownloadTask dt = new DownloadTask();
+            String s = download();
 
-            URL google = new URL("https://www.pcmag.com/picks/best-android-apps");
-            BufferedReader in = new BufferedReader(new InputStreamReader(google.openStream()));
-            String input;
-            StringBuffer stringBuffer = new StringBuffer();
-            while ((input = in.readLine()) != null) {
-                stringBuffer.append(input);
-            }
-            in.close();
-
-
-            String s = stringBuffer.toString();
-
-            String regex = "http(s?)://([\\w-]+\\.)+[\\w-]+(/[\\w- ./]*)+\\.(?:[gG][iI][fF]|[jJ][pP][gG]|[jJ][pP][eE][gG]|[pP][nN][gG]|[bB][mM][pP])";
-            Matcher m = Pattern.compile(regex).matcher(s);
-            String fulldata = "";
-            while (m.find()) {
-                String src = m.group();
-                int startIndex = src.indexOf("src=") + 1;
-                String srcTag = src.substring(startIndex, src.length());
-                fulldata+= srcTag+"\n";
-                images.add(srcTag);
-            }
+//            URL google = new URL("https://www.pcmag.com/picks/best-android-apps");
+//            BufferedReader in = new BufferedReader(new InputStreamReader(google.openStream()));
+//            String input;
+//            StringBuffer stringBuffer = new StringBuffer();
+//            while ((input = in.readLine()) != null) {
+//                stringBuffer.append(input);
+//            }
+//            in.close();
+//            String s = stringBuffer.toString();
 
 
-            String regex2 = "<h2 class=\"order-last md:order-first font-bold font-brand text-lg md:text-xl leading-normal w-full\">(.*?)</h2>";
-            Matcher m2 = Pattern.compile(regex2).matcher(s);
-            String fulldata2 = "";
-            int count = 0;
-            while(m2.find()){
-                String src = m2.group();
-                int startIndex = src.indexOf(">") + 1;
-                int endIndex = src.indexOf("</h2>");
-                String srcTag = src.substring(startIndex, endIndex);
-                names.add(srcTag);
-            }
+            getImagesFromDownloadedURL(s);
+            getNamesFromDownloadedURL(s);
             rrandomm();
 
-            option1.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    if(option1.getText().toString().equalsIgnoreCase(right)){
-                        rightAnswer();
-                        rrandomm();
-                    }else{
-                        wrongAnswer();
-                    }
-                }
-            });
-            option2.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    if(option2.getText().toString().equalsIgnoreCase(right)){
-                        rightAnswer();
-                        rrandomm();
-                    }else{
-                        wrongAnswer();
-                    }
-                }
-            });
-            option3.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    if(option3.getText().toString().equalsIgnoreCase(right)){
-                        rightAnswer();
-                        rrandomm();
-                    }else{
-                        wrongAnswer();
-                    }
-                }
-            });
-            option4.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    if(option4.getText().toString().equalsIgnoreCase(right)){
-                        rightAnswer();
-                        rrandomm();
-                    }else{
-                        wrongAnswer();
-                    }
-                }
-            });
+            btnSetOnClickListener(option1);
+            btnSetOnClickListener(option2);
+            btnSetOnClickListener(option3);
+            btnSetOnClickListener(option4);
+
 
         } catch (Exception e) {
             Log.e("Fetching", "Error Fetching https://www.pcmag.com/picks/best-android-apps");
@@ -144,6 +85,53 @@ public class Level1 extends AppCompatActivity {
         }
 
     }
+
+    public void btnSetOnClickListener(Button btn){
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkCorrectAnswer(btn);
+            }
+        });
+    }
+
+    public void checkCorrectAnswer(Button btn){
+        if(btn.getText().toString().equalsIgnoreCase(right)){
+            rightAnswer();
+            rrandomm();
+        }else{
+            wrongAnswer();
+        }
+    }
+
+    public void getImagesFromDownloadedURL(String s){
+        String regex = "\"(https://i.pcmag.com/imagery/(collection-group-product|roundup-products).*?)\"";
+        Matcher m = Pattern.compile(regex).matcher(s);
+        while (m.find()) {
+            String src = m.group(1);
+            images.add(src);
+        }
+    }
+
+    public void getNamesFromDownloadedURL(String s){
+        String regex = "alt=\"(.*?) Image\"";
+        Matcher m = Pattern.compile(regex).matcher(s);
+        int count = 0;
+        while(m.find()){
+            String src = m.group(1);
+            names.add(src);
+            count++;
+            if(count==103)
+                break;
+        }
+    }
+
+    public String download(){
+        return new DownloadTask().doInBackground("https://www.pcmag.com/picks/best-android-apps");
+    }
+
+
+
 
     public static Bitmap getBitmapFromURL(String src) {
         try {
@@ -163,35 +151,43 @@ public class Level1 extends AppCompatActivity {
         }
     }
 
-    public void rrandomm(){
-        if(count==103){
-            imgnry_timer.setText("GameOver");
-            option1.setOnClickListener(null);
-            option2.setOnClickListener(null);
-            option3.setOnClickListener(null);
-            option4.setOnClickListener(null);
-        }
-        count++;
+    public void stopGame(){
+        option1.setOnClickListener(null);
+        option2.setOnClickListener(null);
+        option3.setOnClickListener(null);
+        option4.setOnClickListener(null);
+    }
+
+    public void shuffleOptions(){
         Random rand = new Random();
-        int first = rand.nextInt(names.size())+12;
-        int second = first-12;
-        image.setImageBitmap(getBitmapFromURL(images.get(first)));
-        right = names.get(second);
-        options.add(right);
-        Log.v("all options",options.toString());
-        while(options.size()<4){
-            int N =rand.nextInt(names.size());
-            if(!names.get(N).equalsIgnoreCase(right))
-                options.add(names.get(N));
-            Log.v("all options",options.toString());
-        }
         int n = rand.nextInt(options.size());
         option1.setText(options.remove(n));
         n = rand.nextInt(options.size());
         option2.setText(options.remove(n));
         n = rand.nextInt(options.size());
         option3.setText(options.remove(n));
-        option4.setText(options.remove(0));
+        n = rand.nextInt(options.size());
+        option4.setText(options.remove(n));
+    }
+
+    public void rrandomm(){
+        if(count==103){
+            stopGame();
+        }
+
+        count++;
+        Random rand = new Random();
+        int first = rand.nextInt(names.size());
+        int second = first;
+        image.setImageBitmap(getBitmapFromURL(images.get(first)));
+        right = names.get(second);
+        options.add(right);
+        while(options.size()<4){
+            int N =rand.nextInt(names.size());
+            if(!names.get(N).equalsIgnoreCase(right))
+                options.add(names.get(N));
+        }
+        shuffleOptions();
     }
 
     public void rightAnswer(){
@@ -201,5 +197,36 @@ public class Level1 extends AppCompatActivity {
         Log.i("correctness", "wrong app");
     }
 
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            String result = "";
+
+            URL url;
+            HttpURLConnection urlConnection = null;
+            try{
+                url = new URL(strings[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+
+                int data = reader.read();
+                while(data!=-1){
+                    char current = (char) data;
+                    result += current;
+                    data=reader.read();
+                }
+
+                in.close();
+                reader.close();
+                return result;
+            }catch(Exception e){
+                e.printStackTrace();
+                return "Failed";
+            }
+
+        }}
 
 }
